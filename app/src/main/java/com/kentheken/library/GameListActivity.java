7103781bf368e770716ab68cd58d492306f74c59
Kenneth Cordero <kencordero@gmail.com>
2014-06-18 16:24:35 -0500

    More setup

--- app/src/main/java/com/kentheken/library/GameListActivity.java
+++ app/src/main/java/com/kentheken/library/GameListActivity.java
@@ -12,7 +12,7 @@ public class GameListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new GameFragment();
+        return new GameListFragment();
     }
 
     @Override
@@ -28,8 +28,7 @@ public class GameListActivity extends SingleFragmentActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/com/kentheken/library/GameListFragment.java
@@ -0,0 +1,20 @@
+package com.kentheken.library;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kcordero on 6/18/2014.
+ */
+public class GameListFragment extends ListFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, parent, savedInstanceState);
+
+        // TODO
+        return view;
+    }
+}
